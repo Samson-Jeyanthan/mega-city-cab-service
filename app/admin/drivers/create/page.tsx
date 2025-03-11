261e0ceb1a5cff9ffc9a37409421ef903997bf6f
Samson <samsonjeyanthan5@gmail.com>
2025-03-11 09:52:44 +0530

    Minor Update

--- app/admin/drivers/create/page.tsx
+++ app/admin/drivers/create/page.tsx
@@ -1,11 +1,13 @@
 import { DriverForm } from "@/components/forms";
+import { getAllManagersAction } from "@/lib/actions/manager.action";
 
-const CreateDriver = () => {
+const CreateDriver = async () => {
+  const results = await getAllManagersAction();
   return (
     <section className="w-full flex-col flex gap-8">
       <h1 className="text-3xl font-bold">Create Driver</h1>
 
-      <DriverForm type="create" />
+      <DriverForm type="create" carDetails={JSON.stringify(results.data)} />
     </section>
   );
 };
--- components/forms/DriverForm.tsx
+++ components/forms/DriverForm.tsx
@@ -9,23 +9,29 @@ import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { usePathname } from "next/navigation";
 import { z } from "zod";
+import { useState } from "react";
+import Image from "next/image";
 
 interface Props {
   type: "edit" | "create";
   driverDetails?: string;
+  carDetails?: string;
 }
 
-const DriverForm = ({ type, driverDetails }: Props) => {
+const DriverForm = ({ type, driverDetails, carDetails }: Props) => {
   const pathname = usePathname();
+  const parsedData = driverDetails ? JSON.parse(driverDetails) : null;
+  const parsedCarDetails = carDetails ? JSON.parse(carDetails) : null;
+  const [selectedCars, setSelectedCars] = useState([]);
 
   const form = useForm<z.infer<typeof DriverSchema>>({
     resolver: zodResolver(DriverSchema),
     defaultValues: {
-      driverName: "",
-      nicNo: "",
-      phoneNo: "",
-      email: "",
-      address: "",
+      driverName: parsedData?.driverName || "",
+      nicNo: parsedData?.nicNo || "",
+      phoneNo: parsedData?.phoneNo || "",
+      email: parsedData?.email || "",
+      address: parsedData?.address || "",
       driverPhoto: [],
     },
   });
@@ -77,6 +83,7 @@ const DriverForm = ({ type, driverDetails }: Props) => {
               <DriverPhotoInput
                 fieldChange={field.onChange}
                 defaultPic="/user-single.png"
+                mediaUrl={parsedData?.driverPhoto}
               />
             )}
           />
@@ -93,6 +100,31 @@ const DriverForm = ({ type, driverDetails }: Props) => {
         <FormInput form={form} inputName="phoneNo" formLabel="Phone Number" />
         <FormInput form={form} inputName="email" formLabel="Email" />
         <FormInput form={form} inputName="address" formLabel="Address" />
+
+        <p className="font-semibold text-sm -mb-2">Select Cars to assign</p>
+        <div className="flex-wrap flex gap-4">
+          {parsedCarDetails?.map((car: any, index: number) => (
+            <div
+              key={index}
+              className="p-4 rounded-lg border flex gap-4 w-[22rem]"
+            >
+              <Image
+                src={car.carPhoto || "/car.png"}
+                alt="car photo"
+                width={200}
+                height={200}
+                className="w-20 h-20 object-cover rounded-md bg-light-700"
+              />
+
+              <div className="flex flex-col gap-2">
+                <p className="font-semibold">
+                  {car.carMade}-{car.carModel}
+                </p>
+                <p>{car.carNo}</p>
+              </div>
+            </div>
+          ))}
+        </div>
         <Button
           className="bg-primary-500 text-light-900 w-full"
           type="submit"
