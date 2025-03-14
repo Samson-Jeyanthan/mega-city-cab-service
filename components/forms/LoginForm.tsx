1f7ce1dbb2b4ef257a1c57976094b5d81c519ae3
Samson <samsonjeyanthan5@gmail.com>
2025-03-14 08:12:54 +0530

    Authentication related functions

--- components/forms/LoginForm.tsx
+++ components/forms/LoginForm.tsx
@@ -7,6 +7,7 @@ import { z } from "zod";
 import { LoginSchema } from "@/lib/validations/admin.validations";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { FormInput } from "../inputs";
+import Link from "next/link";
 
 const LoginForm = () => {
   const form = useForm<z.infer<typeof LoginSchema>>({
@@ -25,7 +26,7 @@ const LoginForm = () => {
     <Form {...form}>
       <form
         onSubmit={form.handleSubmit(onSubmit)}
-        className="flex flex-col gap-4"
+        className="flex flex-col gap-4 w-full"
       >
         <FormInput form={form} inputName="email" formLabel="Email" />
         <FormInput
@@ -34,13 +35,25 @@ const LoginForm = () => {
           formLabel="Password"
           inputType="password"
         />
-        <Button
-          className="bg-primary-500 text-light-900 w-full"
-          type="submit"
-          disabled={form.formState.isSubmitting}
-        >
-          {form.formState.isSubmitting ? "Logging in..." : "Login"}
-        </Button>
+        <footer className="w-full flex flex-col items-center">
+          <p className="text-sm text-light-300 mb-10">
+            Don't have an account?{" "}
+            <Link
+              href="/signup"
+              className="text-primary-500 hover:underline cursor-pointer"
+            >
+              Signup
+            </Link>
+          </p>
+
+          <Button
+            className="bg-primary-500 text-light-900 w-full"
+            type="submit"
+            disabled={form.formState.isSubmitting}
+          >
+            {form.formState.isSubmitting ? "Logging in..." : "Login"}
+          </Button>
+        </footer>
       </form>
     </Form>
   );
--- components/forms/SignupForm.tsx
+++ components/forms/SignupForm.tsx
@@ -1,7 +1,98 @@
 "use client";
 
+import { SignupSchema } from "@/lib/validations/admin.validations";
+import { useForm } from "react-hook-form";
+import { toast } from "sonner";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { usePathname, useRouter } from "next/navigation";
+import { z } from "zod";
+import { Form } from "../ui/form";
+import { FormInput } from "../inputs";
+import { Button } from "../ui/button";
+import Link from "next/link";
+import { customerRegisterAction } from "@/lib/actions/auth.action";
+
 const SignupForm = () => {
-  return <div>SignupForm</div>;
+  const router = useRouter();
+  const form = useForm<z.infer<typeof SignupSchema>>({
+    resolver: zodResolver(SignupSchema),
+    defaultValues: {
+      name: "",
+      nicNo: "",
+      phoneNo: "",
+      email: "",
+      address: "",
+      password: "",
+      confirmPassword: "",
+    },
+  });
+
+  async function onSubmit(values: z.infer<typeof SignupSchema>) {
+    console.log(values);
+
+    let res = {
+      status: "",
+      message: "",
+    };
+
+    try {
+      const res = await customerRegisterAction(values);
+      if (res.status === "200") {
+        toast.success(res.message);
+        router.push("/booking");
+      } else {
+        toast.error(res.message);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  return (
+    <Form {...form}>
+      <form
+        onSubmit={form.handleSubmit(onSubmit)}
+        className="flex flex-col gap-4 w-full"
+      >
+        <FormInput form={form} inputName="name" formLabel="Name" />
+        <FormInput form={form} inputName="nicNo" formLabel="NIC Number" />
+        <FormInput form={form} inputName="phoneNo" formLabel="Phone Number" />
+        <FormInput form={form} inputName="address" formLabel="Address" />
+        <FormInput form={form} inputName="email" formLabel="Email" />
+        <FormInput
+          form={form}
+          inputName="password"
+          formLabel="Password"
+          inputType="password"
+        />
+        <FormInput
+          form={form}
+          inputName="confirmPassword"
+          formLabel="Confirm Password"
+          inputType="password"
+        />
+        <footer className="w-full flex flex-col items-center">
+          <p className="text-sm text-light-300 mb-10">
+            Already have an account?{" "}
+            <Link
+              href="/login"
+              className="text-primary-500 hover:underline cursor-pointer"
+            >
+              Login
+            </Link>
+          </p>
+
+          <Button
+            className="bg-primary-500 text-light-900 w-full"
+            type="submit"
+            disabled={form.formState.isSubmitting}
+          >
+            {form.formState.isSubmitting ? "Registering..." : "Signup"}
+          </Button>
+        </footer>
+      </form>
+    </Form>
+  );
 };
 
 export default SignupForm;
